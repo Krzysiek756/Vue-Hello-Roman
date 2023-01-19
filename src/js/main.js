838e02f14d30650f87c80fc0b4e5f5d8ae53c536
Krzysztof Siek <Krzysztof.Siek93@gmail.com>
2023-01-19 17:32:27 +0100

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,23 @@ const {
 
 const NewComponent = {
   name: 'NewComponent',
-  template: `<h2> Best It vlog on youtube</h2>`
+  data() {
+
+    return {
+      myColor: {
+        color: 'green',
+      },
+    }
+
+  },
+
+  template: `
+  <div>
+  <h2 :style="myColor">Best It vlog on youtube</h2>
+  <label for='color'>Change Color</label>
+  <input name="color" id="color" v-model="myColor.color">
+  </div>
+  `,
 }
 
 createApp({
@@ -15,6 +31,7 @@ createApp({
       style: {
         color: '#fab',
       }
+
     }
   },
 
@@ -28,7 +45,9 @@ createApp({
   // `,
 
   methods: {
-
+    handleClick() {
+      alert('Hello Roman')
+    }
 
 
   },
@@ -40,9 +59,97 @@ createApp({
   },
 
   mounted() {
-    console.log(`The initial count is ${this.count}.`)
+
   }
 }).mount('#app')
 
 
-// v-bind łączy data z templaitem
+// v-bind łączy data z templaitem v-bind:style="myColor" === :style="myColor"
+// v-on === @click 
+// v-model spina input z innym elementem i łączy ja zmienna
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Podstawy
+
+// const {
+//   createApp
+// } = Vue
+
+// const NewComponent = {
+//   name: 'NewComponent',
+//   data() {
+
+//     return {
+//       myColor: {
+//         color: 'green',
+//       },
+//     }
+
+//   },
+
+//   template: `
+//   <div>
+//   <h2 :style="myColor">Best It vlog on youtube</h2>
+//   <label for='color'>Change Color</label>
+//   <input name="color" id="color" v-model="myColor.color">
+//   </div>
+//   `,
+// }
+
+// createApp({
+//   data() { //reaktywność delarujemy różne funckje później łączymy je z template
+//     return {
+//       name: 'Adam',
+
+//       style: {
+//         color: '#fab',
+//       }
+
+//     }
+//   },
+
+//   // template: ` 
+//   //         <div>
+//   //         <h1>Start Program</h1> 
+//   //         <NewComponent />
+//   //         </div>
+
+
+//   // `,
+
+//   methods: {
+//     handleClick() {
+//       alert('Hello Roman')
+//     }
+
+
+//   },
+//   computed: {
+
+//   },
+//   components: {
+//     NewComponent
+//   },
+
+//   mounted() {
+
+//   }
+// }).mount('#app')
+
+
+// // v-bind łączy data z templaitem v-bind:style="myColor" === :style="myColor"
+// // v-on === @click 
+// // v-model spina input z innym elementem i łączy ja zmienna
