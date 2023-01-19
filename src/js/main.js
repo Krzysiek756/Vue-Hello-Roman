0c2c957f37a067d3c941c5a840552be1db9dcab0
Krzysztof Siek <Krzysztof.Siek93@gmail.com>
2023-01-19 16:37:31 +0100

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,28 @@ const {
   createApp
 } = Vue
 
+const NewComponent = {
+  name: 'NewComponent',
+  template: `<h2> Best It vlog on youtube</h2>`
+}
+
 createApp({
   data() { //reaktywność delarujemy różne funckje później łączymy je z template
     return {
       name: 'Adam',
+
+      style: {
+        color: '#fab',
+      }
     }
   },
 
-  // template: ` // używamy Puga do template
-  //       <h1>Start Program</h1> 
+  // template: ` 
+  //         <div>
+  //         <h1>Start Program</h1> 
+  //         <NewComponent />
+  //         </div>
+
 
   // `,
 
@@ -22,8 +35,14 @@ createApp({
   computed: {
 
   },
+  components: {
+    NewComponent
+  },
 
   mounted() {
     console.log(`The initial count is ${this.count}.`)
   }
 }).mount('#app')
+
+
+// v-bind łączy data z templaitem
